c1c10cf53e8b6679802c60a127382940490f8d4a
mugishamoses <m.mugisha@alustudent.com>
2025-10-19 23:55:20 +0200

    Made some changes

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -7,7 +7,7 @@ import sys
 if __name__ == '__main__':
     """gather data from an api"""
     response_todos = requests.get(
-        "https://jsonplaceholder.typicode.com/user/" + sys.argv[1] + "/todos"
+        "https://jsonplaceholder.typicode.com/users/" + sys.argv[1] + "/todos"
     )
     data = response_todos.json()
     completed_tasks = []
@@ -18,6 +18,7 @@ if __name__ == '__main__':
     user = requests.get(
         "https://jsonplaceholder.typicode.com/users/" + sys.argv[1]
     ).json()
-    print("Employee {} is done with tasks({}/{}):".
+    print("Employee {} is done with tasks({}/{}): ".
           format(user["name"], len(completed_tasks), len(data)))
-    [print("\t " + task) for task in completed_tasks]
+    for task in completed_tasks:
+        print("\t " + task)
